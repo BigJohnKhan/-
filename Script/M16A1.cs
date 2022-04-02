9992528d447fabc07bce533fd6def67548a8d5f1
BigJohnKhan <90232727+BigJohnKhan@users.noreply.github.com>
2022-04-02 20:04:01 +0800

    Add files via upload

---
+++ Script/M16A1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string Name;
+    public int atk;
+    public int hp;
+    public int dod;
+    public int acc;
+    public int mov;
+    public int rag;
+    public string skl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
